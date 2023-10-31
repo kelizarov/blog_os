0d59abbacc46732106cbb0d842c1da8956693fa3
Kirill Elizarov <kirillthetee@gmail.com>
2023-10-31 16:49:11 +0700

    Added basic CPU Interrupt handler function to catch CPU exceptions.

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,11 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
 
+pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
 
@@ -23,6 +25,10 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
     }
+}
+
+pub fn init(){
+    interrupts::init_idt();
 }
 
 pub trait Testable {
@@ -58,6 +64,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
